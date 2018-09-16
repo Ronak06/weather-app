2f2723a7698abf59373dec07fb0cc537e6dca33a
Ronak Patel <ronak1.patel@ryerson.ca>
2018-09-16 00:16:34 -0400

    Created and added api key to access weather information

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = "f55fbb38bc222d84aa979f488871d9f7";
+
 class App extends React.Component {
+    getWeather = async () => {
+        const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+        const data = await api_call.json();
+
+    }
+
     render() {
         return(
             <div>
