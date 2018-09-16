e768ebf12b306b80c0f3b9b939e3e7eb1e91face
Ronak Patel <ronak1.patel@ryerson.ca>
2018-09-16 00:58:27 -0400

    Basic functionality of weather app completed

--- src/App.js
+++ src/App.js
@@ -23,23 +23,43 @@ class App extends React.Component {
         const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
         const data = await api_call.json();
 
-        console.log(data);
-        this.setState({
-            temperature: data.main.temp,
-            city: data.name,
-            country: data.sys.country,
-            humidity: data.main.humidity,
-            description: data.weather[0].description,
-            error: ""
-        });
+        if(city && country) {
+            console.log(data);
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: ""
+            });
+        } else {
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please enter a city and country in the textboxes above."
+            });
+        }
     }
 
     render() {
         return(
             <div>
                 <Titles />
+
                 <Form getWeather={this.getWeather}/>
-                <Weather />
+                
+                <Weather 
+                temperature={this.state.temperature}
+                city={this.state.city}
+                country={this.state.country}
+                humidity={this.state.humidity}
+                description={this.state.description}
+                error={this.state.error}
+                />
             </div>
         );
     }
