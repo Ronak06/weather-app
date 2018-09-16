7d2f8c4bfa3434bece5ef893c03d364b169e67b3
Ronak Patel <ronak1.patel@ryerson.ca>
2018-09-16 00:36:48 -0400

    set variables and fetched weather info

--- src/App.js
+++ src/App.js
@@ -7,17 +7,38 @@ import Weather from './components/Weather';
 const API_KEY = "f55fbb38bc222d84aa979f488871d9f7";
 
 class App extends React.Component {
-    getWeather = async () => {
-        const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+    }
+
+    getWeather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
         const data = await api_call.json();
 
+        console.log(data);
+        this.setState({
+            temperature: data.main.temp,
+            city: data.name,
+            country: data.sys.country,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            error: ""
+        });
     }
 
     render() {
         return(
             <div>
                 <Titles />
-                <Form />
+                <Form getWeather={this.getWeather}/>
                 <Weather />
             </div>
         );
