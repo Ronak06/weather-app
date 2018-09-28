3a274c7cb4e2cb2f6c0167a557fa15d743e8b2c7
Ronak Patel <ronak1.patel@ryerson.ca>
2018-09-27 22:55:12 -0400

    Changed background color, padding between input boxes, added color to error message, modified Weather title

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Titles = () => (
     <div>
-        <h1>Weather App</h1>
+        <h1>Daily Weather Forecast</h1>
         <p>Want to find out about the weather today? Enter a city and country!</p>
     </div>  
 );
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Weather = (props) => (
     <div>
-        { props.city && props.country && <p>Location: { props.city }, { props.country }</p> }
-        { props.temperature && <p>Temperature: { props.temperature } K</p> }
-        { props.high && <p>High: { props.high } K</p> }
-        { props.low && <p>Low: { props.low } K</p> }
-        { props.humidity && <p>Humidity: { props.humidity }</p> }
-        { props.description && <p>Conditions: { props.description }</p> }
-        { props.error && <p>{ props.error }</p> }
+        { props.city && props.country && <p id="location">Location: { props.city }, { props.country }</p> }
+        { props.temperature && <p id="temperature">Temperature: { props.temperature } K</p> }
+        { props.high && <p id="high">High: { props.high } K</p> }
+        { props.low && <p id="low">Low: { props.low } K</p> }
+        { props.humidity && <p id="humidity">Humidity: { props.humidity }</p> }
+        { props.description && <p id="conditions">Conditions: { props.description }</p> }
+        { props.error && <p id="error">{ props.error }</p> }
     </div>
 );
 
